9426b731298bc0a9f667f56a9774ea5906f8fbfd
thiagonego <thiagonego@gmail.com>
2016-05-19 15:41:17 -0300

    Incrementando versão para produção

--- src/main/java/br/com/conductor/pier/api/v1/CartaoApi.java
+++ src/main/java/br/com/conductor/pier/api/v1/CartaoApi.java
@@ -10,8 +10,6 @@ import java.util.*;
 
 import br.com.conductor.pier.api.v1.model.CancelarCartaoResponse;
 import br.com.conductor.pier.api.v1.model.ConsultarCartaoResponse;
-import br.com.conductor.pier.api.v1.model.ConsultarExtratoContaResponse;
-import br.com.conductor.pier.api.v1.model.ConsultarSaldoLimitesResponse;
 import br.com.conductor.pier.api.v1.model.DesbloquearCartaoResponse;
 
 
@@ -45,40 +43,35 @@ public class CartaoApi {
 
   
   /**
-   * /contas/{idConta}/cartoes/{idCartao}/cancelar
-   * Cancelar um determinado cart\u00C3\u00A3o
+   * /contas/{idConta}/cartoes/{idCartao}/bloquear
+   * Bloquear um determinado cart\u00C3\u00A3o
    * @param idConta ID da Conta
    * @param idCartao ID do Cart\u00C3\u00A3o que deseja cancelar
-   * @param motivo Motivo do cancelamento
-   * @param observacao Alguma observa\u00C3\u00A7\u00C3\u00A3o para o cancelamento
+   * @param motivo Motivo do bloqueio
+   * @param observacao Alguma observa\u00C3\u00A7\u00C3\u00A3o para o bloqueio
    * @return CancelarCartaoResponse
    */
-  public CancelarCartaoResponse  cancelarCartaoUsingPOST (Integer idConta, Integer idCartao, Integer motivo, String observacao) throws ApiException {
+  public CancelarCartaoResponse  bloquearCartaoUsingPOST (Integer idConta, Integer idCartao, Integer motivo, String observacao) throws ApiException {
     Object postBody = null;
     
     // verify the required parameter 'idConta' is set
     if (idConta == null) {
-       throw new ApiException(400, "Missing the required parameter 'idConta' when calling cancelarCartaoUsingPOST");
+       throw new ApiException(400, "Missing the required parameter 'idConta' when calling bloquearCartaoUsingPOST");
     }
     
     // verify the required parameter 'idCartao' is set
     if (idCartao == null) {
-       throw new ApiException(400, "Missing the required parameter 'idCartao' when calling cancelarCartaoUsingPOST");
+       throw new ApiException(400, "Missing the required parameter 'idCartao' when calling bloquearCartaoUsingPOST");
     }
     
     // verify the required parameter 'motivo' is set
     if (motivo == null) {
-       throw new ApiException(400, "Missing the required parameter 'motivo' when calling cancelarCartaoUsingPOST");
-    }
-    
-    // verify the required parameter 'observacao' is set
-    if (observacao == null) {
-       throw new ApiException(400, "Missing the required parameter 'observacao' when calling cancelarCartaoUsingPOST");
+       throw new ApiException(400, "Missing the required parameter 'motivo' when calling bloquearCartaoUsingPOST");
     }
     
 
     // create path and map variables
-    String path = "/v1/contas/{idConta}/cartoes/{idCartao}/cancelar".replaceAll("\\{format\\}","json").replaceAll("\\{" + "idConta" + "\\}", apiInvoker.escapeString(idConta.toString())).replaceAll("\\{" + "idCartao" + "\\}", apiInvoker.escapeString(idCartao.toString()));
+    String path = "/v1/contas/{idConta}/cartoes/{idCartao}/bloquear".replaceAll("\\{format\\}","json").replaceAll("\\{" + "idConta" + "\\}", apiInvoker.escapeString(idConta.toString())).replaceAll("\\{" + "idCartao" + "\\}", apiInvoker.escapeString(idCartao.toString()));
 
     // query params
     List<Pair> queryParams = new ArrayList<Pair>();
@@ -253,144 +246,6 @@ public class CartaoApi {
   }
   
   /**
-   * /contas/{idConta}/cartoes/{idCartao}/faturas
-   * Consulte os extratos/faturas do cart\u00C3\u00A3o de uma determinada conta
-   * @param idConta ID da Conta
-   * @param idCartao ID do Cart\u00C3\u00A3o que deseja consultar o extrato
-   * @param dataVencimento Data limite para o vencimento das transa\u00C3\u00A7\u00C3\u00B5es
-   * @return ConsultarExtratoContaResponse
-   */
-  public ConsultarExtratoContaResponse  consultarExtratoFaturasUsingGET (Integer idConta, Integer idCartao, String dataVencimento) throws ApiException {
-    Object postBody = null;
-    
-    // verify the required parameter 'idConta' is set
-    if (idConta == null) {
-       throw new ApiException(400, "Missing the required parameter 'idConta' when calling consultarExtratoFaturasUsingGET");
-    }
-    
-    // verify the required parameter 'idCartao' is set
-    if (idCartao == null) {
-       throw new ApiException(400, "Missing the required parameter 'idCartao' when calling consultarExtratoFaturasUsingGET");
-    }
-    
-    // verify the required parameter 'dataVencimento' is set
-    if (dataVencimento == null) {
-       throw new ApiException(400, "Missing the required parameter 'dataVencimento' when calling consultarExtratoFaturasUsingGET");
-    }
-    
-
-    // create path and map variables
-    String path = "/v1/contas/{idConta}/cartoes/{idCartao}/faturas".replaceAll("\\{format\\}","json").replaceAll("\\{" + "idConta" + "\\}", apiInvoker.escapeString(idConta.toString())).replaceAll("\\{" + "idCartao" + "\\}", apiInvoker.escapeString(idCartao.toString()));
-
-    // query params
-    List<Pair> queryParams = new ArrayList<Pair>();
-    // header params
-    Map<String, String> headerParams = new HashMap<String, String>();
-    // form params
-    Map<String, String> formParams = new HashMap<String, String>();
-
-    
-    queryParams.addAll(ApiInvoker.parameterToPairs("", "dataVencimento", dataVencimento));
-    
-
-    
-
-    String[] contentTypes = {
-      "application/json"
-    };
-    String contentType = contentTypes.length > 0 ? contentTypes[0] : "application/json";
-
-    if (contentType.startsWith("multipart/form-data")) {
-      // file uploading
-      MultipartEntityBuilder builder = MultipartEntityBuilder.create();
-      
-
-      HttpEntity httpEntity = builder.build();
-      postBody = httpEntity;
-    } else {
-      // normal form params
-      
-    }
-
-    try {
-      String response = apiInvoker.invokeAPI(basePath, path, "GET", queryParams, postBody, headerParams, formParams, contentType);
-      if(response != null){
-        return (ConsultarExtratoContaResponse) ApiInvoker.deserialize(response, "", ConsultarExtratoContaResponse.class);
-      }
-      else {
-        return null;
-      }
-    } catch (ApiException ex) {
-      throw ex;
-    }
-  }
-  
-  /**
-   * /contas/{idConta}/cartoes/{idCartao}/limites
-   * Consulte os limites de um determinado cart\u00C3\u00A3o
-   * @param idConta ID da Conta
-   * @param idCartao ID do Cart\u00C3\u00A3o que deseja consultar o saldo/limite
-   * @return ConsultarSaldoLimitesResponse
-   */
-  public ConsultarSaldoLimitesResponse  consultarSaldosLimitesUsingGET (Integer idConta, Integer idCartao) throws ApiException {
-    Object postBody = null;
-    
-    // verify the required parameter 'idConta' is set
-    if (idConta == null) {
-       throw new ApiException(400, "Missing the required parameter 'idConta' when calling consultarSaldosLimitesUsingGET");
-    }
-    
-    // verify the required parameter 'idCartao' is set
-    if (idCartao == null) {
-       throw new ApiException(400, "Missing the required parameter 'idCartao' when calling consultarSaldosLimitesUsingGET");
-    }
-    
-
-    // create path and map variables
-    String path = "/v1/contas/{idConta}/cartoes/{idCartao}/limites".replaceAll("\\{format\\}","json").replaceAll("\\{" + "idConta" + "\\}", apiInvoker.escapeString(idConta.toString())).replaceAll("\\{" + "idCartao" + "\\}", apiInvoker.escapeString(idCartao.toString()));
-
-    // query params
-    List<Pair> queryParams = new ArrayList<Pair>();
-    // header params
-    Map<String, String> headerParams = new HashMap<String, String>();
-    // form params
-    Map<String, String> formParams = new HashMap<String, String>();
-
-    
-
-    
-
-    String[] contentTypes = {
-      "application/json"
-    };
-    String contentType = contentTypes.length > 0 ? contentTypes[0] : "application/json";
-
-    if (contentType.startsWith("multipart/form-data")) {
-      // file uploading
-      MultipartEntityBuilder builder = MultipartEntityBuilder.create();
-      
-
-      HttpEntity httpEntity = builder.build();
-      postBody = httpEntity;
-    } else {
-      // normal form params
-      
-    }
-
-    try {
-      String response = apiInvoker.invokeAPI(basePath, path, "GET", queryParams, postBody, headerParams, formParams, contentType);
-      if(response != null){
-        return (ConsultarSaldoLimitesResponse) ApiInvoker.deserialize(response, "", ConsultarSaldoLimitesResponse.class);
-      }
-      else {
-        return null;
-      }
-    } catch (ApiException ex) {
-      throw ex;
-    }
-  }
-  
-  /**
    * /contas/{idConta}/cartoes/{idCartao}/desbloquear
    * Desbloquear cart\u00C3\u00A3o de uma determinada conta
    * @param idConta ID da Conta
@@ -409,11 +264,6 @@ public class CartaoApi {
     // verify the required parameter 'idCartao' is set
     if (idCartao == null) {
        throw new ApiException(400, "Missing the required parameter 'idCartao' when calling desbloquearCartaoUsingPOST");
-    }
-    
-    // verify the required parameter 'codigoSegurancao' is set
-    if (codigoSegurancao == null) {
-       throw new ApiException(400, "Missing the required parameter 'codigoSegurancao' when calling desbloquearCartaoUsingPOST");
     }
     
 
--- src/main/java/br/com/conductor/pier/api/v1/ContaApi.java
+++ src/main/java/br/com/conductor/pier/api/v1/ContaApi.java
@@ -10,6 +10,8 @@ import java.util.*;
 
 import br.com.conductor.pier.api.v1.model.ConsultarContaResponse;
 import br.com.conductor.pier.api.v1.model.ContaResponse;
+import br.com.conductor.pier.api.v1.model.ConsultarExtratoContaResponse;
+import br.com.conductor.pier.api.v1.model.ConsultarSaldoLimitesResponse;
 
 
 import org.apache.http.HttpEntity;
@@ -156,6 +158,132 @@ public class ContaApi {
       String response = apiInvoker.invokeAPI(basePath, path, "GET", queryParams, postBody, headerParams, formParams, contentType);
       if(response != null){
         return (ContaResponse) ApiInvoker.deserialize(response, "", ContaResponse.class);
+      }
+      else {
+        return null;
+      }
+    } catch (ApiException ex) {
+      throw ex;
+    }
+  }
+  
+  /**
+   * /contas/{idConta}/faturas
+   * Consulte os extratos/faturas de uma determinada conta
+   * @param idConta ID da Conta
+   * @param dataVencimento Data limite para o vencimento das transa\u00C3\u00A7\u00C3\u00B5es
+   * @return ConsultarExtratoContaResponse
+   */
+  public ConsultarExtratoContaResponse  consultarExtratoFaturasUsingGET (Integer idConta, String dataVencimento) throws ApiException {
+    Object postBody = null;
+    
+    // verify the required parameter 'idConta' is set
+    if (idConta == null) {
+       throw new ApiException(400, "Missing the required parameter 'idConta' when calling consultarExtratoFaturasUsingGET");
+    }
+    
+    // verify the required parameter 'dataVencimento' is set
+    if (dataVencimento == null) {
+       throw new ApiException(400, "Missing the required parameter 'dataVencimento' when calling consultarExtratoFaturasUsingGET");
+    }
+    
+
+    // create path and map variables
+    String path = "/v1/contas/{idConta}/faturas".replaceAll("\\{format\\}","json").replaceAll("\\{" + "idConta" + "\\}", apiInvoker.escapeString(idConta.toString()));
+
+    // query params
+    List<Pair> queryParams = new ArrayList<Pair>();
+    // header params
+    Map<String, String> headerParams = new HashMap<String, String>();
+    // form params
+    Map<String, String> formParams = new HashMap<String, String>();
+
+    
+    queryParams.addAll(ApiInvoker.parameterToPairs("", "dataVencimento", dataVencimento));
+    
+
+    
+
+    String[] contentTypes = {
+      "application/json"
+    };
+    String contentType = contentTypes.length > 0 ? contentTypes[0] : "application/json";
+
+    if (contentType.startsWith("multipart/form-data")) {
+      // file uploading
+      MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+      
+
+      HttpEntity httpEntity = builder.build();
+      postBody = httpEntity;
+    } else {
+      // normal form params
+      
+    }
+
+    try {
+      String response = apiInvoker.invokeAPI(basePath, path, "GET", queryParams, postBody, headerParams, formParams, contentType);
+      if(response != null){
+        return (ConsultarExtratoContaResponse) ApiInvoker.deserialize(response, "", ConsultarExtratoContaResponse.class);
+      }
+      else {
+        return null;
+      }
+    } catch (ApiException ex) {
+      throw ex;
+    }
+  }
+  
+  /**
+   * /contas/{idConta}/limites
+   * Consulte os limites de uma determinada conta
+   * @param idConta ID da Conta
+   * @return ConsultarSaldoLimitesResponse
+   */
+  public ConsultarSaldoLimitesResponse  consultarSaldosLimitesUsingGET (Integer idConta) throws ApiException {
+    Object postBody = null;
+    
+    // verify the required parameter 'idConta' is set
+    if (idConta == null) {
+       throw new ApiException(400, "Missing the required parameter 'idConta' when calling consultarSaldosLimitesUsingGET");
+    }
+    
+
+    // create path and map variables
+    String path = "/v1/contas/{idConta}/limites".replaceAll("\\{format\\}","json").replaceAll("\\{" + "idConta" + "\\}", apiInvoker.escapeString(idConta.toString()));
+
+    // query params
+    List<Pair> queryParams = new ArrayList<Pair>();
+    // header params
+    Map<String, String> headerParams = new HashMap<String, String>();
+    // form params
+    Map<String, String> formParams = new HashMap<String, String>();
+
+    
+
+    
+
+    String[] contentTypes = {
+      "application/json"
+    };
+    String contentType = contentTypes.length > 0 ? contentTypes[0] : "application/json";
+
+    if (contentType.startsWith("multipart/form-data")) {
+      // file uploading
+      MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+      
+
+      HttpEntity httpEntity = builder.build();
+      postBody = httpEntity;
+    } else {
+      // normal form params
+      
+    }
+
+    try {
+      String response = apiInvoker.invokeAPI(basePath, path, "GET", queryParams, postBody, headerParams, formParams, contentType);
+      if(response != null){
+        return (ConsultarSaldoLimitesResponse) ApiInvoker.deserialize(response, "", ConsultarSaldoLimitesResponse.class);
       }
       else {
         return null;
