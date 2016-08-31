e42d145297371c930f2d21c50c6ecfd770366ae4
Filipe Germano <filipe.germano@conductor.com.br>
2016-08-31 10:19:30 -0300

    Ajuste de Nomes de Objetos, para geração do Swagger.

--- src/main/java/br/com/conductor/pier/api/v1_1/StatusCartaoApi.java
+++ src/main/java/br/com/conductor/pier/api/v1_1/StatusCartaoApi.java
@@ -20,7 +20,7 @@ import java.util.HashMap;
 import java.io.File;
 
 
-public class StatusCartoApi {
+public class StatusCartaoApi {
   String basePath = "https://localhost/";
   ApiInvoker apiInvoker = ApiInvoker.getInstance();
 
